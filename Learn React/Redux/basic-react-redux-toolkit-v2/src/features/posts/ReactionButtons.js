91eb71e9db44ce7da2c449de6759d27070ad1c67
Blue Teeth <daominhtri1000@gmail.com>
2023-08-23 18:05:21 +0700

    Update sample project for learning redux

---
+++ Learn React/Redux/basic-react-redux-toolkit-v2/src/features/posts/ReactionButtons.js
@@ -0,0 +1,33 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const reactionEmoji = {
+    thumbsUp: '👍',
+    wow: '😮',
+    heart: '❤️',
+    rocket: '🚀',
+    coffee: '☕'
+}
+
+const ReactionButtons = ({ post }) => {
+    const dispatch = useDispatch()
+
+    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+        // console.log(`name value: ${name}`);
+        return (
+            <button
+                key={name}
+                type="button"
+                className="reactionButton"
+                onClick={() =>
+                    dispatch(reactionAdded({ postId: post.id, reaction: name }))
+                }
+            >
+                {emoji} {post.reactions[name]}
+            </button>
+        )
+    })
+
+    return <div>{reactionButtons}</div>
+}
+export default ReactionButtons
